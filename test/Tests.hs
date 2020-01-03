bb8fc0a2859dedc639cead22d6e28357040640ca
Kai Gay <gayk@oregonstate.edu>
2020-01-03 14:51:54 -0800

    while loop!

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,4 +1,4 @@
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["-isrc", "src/Language/Syntax.hs", "src/Runtime/Eval.hs"] 
+main = doctest ["-isrc", "src/Language/Syntax.hs", "src/Runtime/Eval.hs", "src/Parser/Parser.hs"] 
