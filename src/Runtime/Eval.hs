7138c10d1b2b7e0aa0e9f1976cbf88dad70a8411
Kai Gay <gayk@oregonstate.edu>
2020-01-05 00:08:07 -0800

    it works?

--- src/Runtime/Eval.hs
+++ src/Runtime/Eval.hs
@@ -6,10 +6,11 @@ import Control.Monad
 import Data.Array
 import Control.Monad.Reader
 import Control.Monad.Except
-
+import Debug.Trace
 type Env = [(Name, V)]
 
-data V = Simple Expr | Fun [Name] Expr
+data V = Simple Expr Env | Fun [Name] Expr Env
+  deriving Show
 
 data Val = Vi Integer
          | Vb Bool
@@ -26,11 +27,13 @@ unpackBool (Vb b) = b
 unpackBool _ = undefined
 
 bindings :: [ValDef] -> Env
-bindings vs = map bind vs
+bindings vs = map (bind x) vs
+  where
+    x = map (bind []) vs
 
-bind :: ValDef -> (Name, V)
-bind (Val _ (Veq n e)) = (n, Simple e)
-bind (Val _ (Feq n (Pars ls) e)) = (n, Fun ls e)
+bind :: Env -> ValDef -> (Name, V)
+bind env (Val _ (Veq n e)) = (n, Simple e env)
+bind env (Val _ (Feq n (Pars ls) e)) = (n, Fun ls e env)
 -- bval?
 -- builtins
 
@@ -42,47 +45,7 @@ builtins :: [(Name, [Expr] -> Eval Val)]
 builtins = [("input", input)]
 
 type Eval a = ReaderT Env (IO) a
-
-evalBinOp :: Op -> Expr -> Expr -> Eval Val 
-evalBinOp Plus l r  = evalNumOp (+) l r  
-evalBinOp Minus l r = evalNumOp (-) l r
-evalBinOp Times l r = evalNumOp (*) l r
-evalBinOp Div l r   = evalNumOp div l r 
-evalBinOp Mod l r   = evalNumOp mod l r
-evalBinOp Equiv l r = evalBoolOp (==) l r
-evalBinOp Or l r    = evalBoolOp (&&) l r
-evalBinOp Less l r  = evalBoolOp (<) l r
-evalBinOp And l r   = evalBoolOp (||) l r 
-evalBinOp Xor l r   = evalBoolOp (/=) l r
-
--- |evaluates numerical operations 
-evalNumOp :: (Integer -> Integer -> Integer) -> Expr -> Expr -> Eval Val 
-evalNumOp f l r = do
-                     v1 <- eval l 
-                     v2 <- eval r 
-                     case (v1, v2) of 
-                        (Vi l', Vi r') -> return (Vi (f l' r'))
-                        _ -> return $ Err $ "Could not do numerical operation on " ++ (show l) ++ " to " ++ (show r)  
-
--- |evaluates boolean operations 
-evalBoolOp :: (Bool -> Bool -> Bool) -> Expr -> Expr -> Eval Val 
-evalBoolOp f l r = do
-                     v1 <- eval l 
-                     v2 <- eval r 
-                     case (v1, v2) of 
-                        (Vb l', Vb r') -> return (Vb (f l' r'))
-                        _ -> return $ Err $ "Could not do boolean operation on " ++ (show l) ++ " to " ++ (show r)  
-
 -- | Evaluate an expression in the Eval Monad
--- 
--- >>> run [] (Binop Equiv (B True) (Binop And (B True) (B True)))
--- Result: Vb True 
--- 
--- >>> run [] (Binop Plus (Binop Minus (I 1) (I 1)) (Binop Times (I 2) (I 3)))
--- Result: Vi 6  
--- 
--- >>> run [] (Binop Plus (B True) (Binop Times (I 2) (I 3)))
--- Result: Err ...  
 eval :: Expr -> Eval Val
 eval (I i) = return $ Vi i
 eval (B b) = return $ Vb b
@@ -91,28 +54,41 @@ eval (Tuple es) = (sequence (map eval es)) >>= (return . Vt)
 eval (Ref n) = do
   e <- ask
   case lookup n e of
-        Just (Simple v) -> eval v
+        Just (Simple v e') -> (trace $ "evaling " ++ show v) $ local (const $ e') (eval v)
         _ -> return $ Err $ "Variable " ++ n ++ " undefined"
 eval (App n es) = do
   env <- ask
+  args <- return $  map (\x -> Simple x env) es
+  (trace $ show env) $ return ()
   case lookup n env of
-        Just (Fun params e) -> local ((++) (zip params args)) (eval e)
-        Nothing -> case lookup n builtins of
-                Just (f) -> f es
-                Nothing -> return $ Err ""
-  where
-    args = map Simple es
-eval (Let n e1 e2) = local ((:) (n, Simple e1)) (eval e2)
+    Just (Fun params e env') -> local (const $ ((zip params args)) ++ env') (eval e)
+    Nothing -> case lookup n builtins of
+        Just (f) -> f es
+        Nothing -> return $ Err $ "Couldn't find " ++ n ++ "in enviroment!"
+eval (Let n e1 e2) = do
+  env <- ask
+  local ((:) (n, Simple e1 env)) (eval e2)
 eval (If p e1 e2) = do
   b <- unpackBool <$> (eval p)
   if b then eval e1 else eval e2
-eval (Binop o l r) = evalBinOp o l r 
 eval (While p f x) = do
   b <- eval (App p [x])
   case b of
     (Vb b) -> if b then eval (While p f (App f [x])) else eval x
     _ -> undefined
  
+eval (Binop Plus e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (Vi x, Vi y) -> return $ Vi (x + y)
+eval (Binop Less e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (Vi i1, Vi i2) -> return $ Vb (i1 < i2)
+
+
 -- | evaluate an expression and run it
 -- >>> run [] (I 2)
 -- Result: Vi 2
@@ -120,9 +96,9 @@ eval (While p f x) = do
 -- Result: Vt [Vi 2,Vi 3,Vi 4]
 -- >>> run [] (Let "x" (I 2) (Ref "x"))
 -- Result: Vi 2
+
 run :: Env -> Expr -> IO ()
 run env e = do
   v <- (runReaderT (eval e) env)
   putStrLn ("Result: " ++ show v)
-
 enviroment = [("ten", (Fun ["x"] (Binop Less (Ref "x") (I 10)))), ("succ", (Fun ["x"] (Binop Plus (Ref "x") (I 1))))]
