795418e7402529457ca7c0192494e8da61de82a0
Kai Gay <gayk@oregonstate.edu>
2020-04-06 15:27:00 -0700

    IO exceptions

--- src/API/SaveFile.hs
+++ src/API/SaveFile.hs
@@ -11,11 +11,14 @@ module API.SaveFile (handleSaveFile) where
 import API.JSONData
 import Servant
 import Control.Monad.IO.Class
+import Control.Exception hiding (Handler)
 
 -- TODO Change SpielResponse to SpielOK/SpielError
 
 handleSaveFile :: SpielFile -> Handler SpielResponses
 handleSaveFile (SpielFile fn contents) = liftIO $ do
     -- TODO needs to check if this file was successfully written, rather than just assuming it is (@montymxb)
-    writeFile (fn ++ ".bgl") contents
-    return [(Log (fn ++ ".bgl" ++ " written successfully"))]
+    success <- try $ writeFile (fn ++ ".bgl") contents :: IO (Either IOException ())
+    case success of
+      Right _ -> return [(Log (fn ++ ".bgl" ++ " written successfully"))]
+      Left e -> return [(Log ("Exception: " ++ displayException e))]
